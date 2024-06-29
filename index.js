68d6fcc855e631e7847b06a5bfca89fbe29951a0
Abraham Garcia <adolfo.91.garcia@gmail.com>
2024-06-28 23:56:34 -0300

    proyect ended

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+import express from 'express';
 import Replicate from 'replicate';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/generate', async (req, res) => {
+
+    const { prompt } = req.body;
+
+    if (!prompt) {
+        return res.status(400).json({ error: "Prompt is required" });
+    }
+
+    const replicate = new Replicate({
+        auth: process.env.REPLICATE_API_KEY,
+    });
+
+
+    const input = {
+        cfg: 3.5,
+        steps: 28,
+        prompt: prompt,
+        aspect_ratio: "1:1",
+        output_format: "webp",
+        output_quality: 90,
+        negative_prompt: "",
+        prompt_strength: 0.85
+    };
+
+    const output = await replicate.run("stability-ai/stable-diffusion-3", { input });
+    console.log(output);
+
+    res.json(output);
+
+    res.send('Generando');
+})
+
+app.listen(3000, () => {
+    console.log("Server running in port 3000");
+})
 
 console.log("ss")
